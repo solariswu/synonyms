c5b182316a40fe8ffb65e27c682a6b161628a3cf
yunganw <solariswu@gmail.com>
2019-11-07 17:14:39 +1100

    add toggleButton

--- src/App.js
+++ src/App.js
@@ -12,18 +12,40 @@ import { ToggleButtonGroup, ToggleButton } from 'react-bootstrap';
 Amplify.configure(awsconfig);
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {answer: ''};
+      }
+
+    handleAnswer = (answer) => {
+        this.setState({
+            answer: answer
+          });
+    }
 
     render() {
         Amplify.Logger.LOG_LEVEL = 'VERBOSE';
+
+        const Hint = ({ content, correctAnswer } ) => {
+            if (this.state.answer.length > 0)  
+                if (this.state.answer !== correctAnswer)  
+                    return ( <div> Nope! <br/> {content} </div> )
+                else 
+                    return (<div> Correct ! </div>)
+            
+            return (<div/>)
+        } 
+
         const ListView = ({ items }) => (
             <div>
-                <h3>All Words</h3>
+                <h3>Please select the most <strong>similar</strong> word to the following word </h3>
+                <h3> ({items[0].base}) </h3>
                 <ToggleButtonGroup name='answers'>
-                    <ToggleButton value={items[0].A}> {items[0].A} </ToggleButton>
-                    <ToggleButton value={items[0].B}> {items[0].B} </ToggleButton>
-                    <ToggleButton value={items[0].C}> {items[0].C} </ToggleButton>
-                    <ToggleButton value={items[0].D}> {items[0].D} </ToggleButton>
-                    <ToggleButton value={items[0].E}> {items[0].E} </ToggleButton>
+                    <ToggleButton value={items[0].A} onChange={() => this.handleAnswer(items[0].A)}> {items[0].A} </ToggleButton>
+                    <ToggleButton value={items[0].B} onChange={() => this.handleAnswer(items[0].B)}> {items[0].B} </ToggleButton>
+                    <ToggleButton value={items[0].C} onChange={() => this.handleAnswer(items[0].C)}> {items[0].C} </ToggleButton>
+                    <ToggleButton value={items[0].D} onChange={() => this.handleAnswer(items[0].D)}> {items[0].D} </ToggleButton>
+                    <ToggleButton value={items[0].E} onChange={() => this.handleAnswer(items[0].E)}> {items[0].E} </ToggleButton>
                 </ToggleButtonGroup>
             </div>
         );
@@ -33,7 +55,13 @@ class App extends Component {
                 {({ data: { listSynonyms }, loading, errors }) => {
                     if (loading || !listSynonyms) return (<h3>Loading...</h3>);
                     if (errors.lenth > 0 ) return (<h3>Error</h3>);
-                    return (<ListView items={listSynonyms.items} /> );
+                    return (
+                        <div>
+                            <ListView items={listSynonyms.items} /> 
+                            <Hint content={listSynonyms.items[0].Hint}
+                                  correctAnswer={listSynonyms.items[0].Answer} />
+                        </div>
+                    );
                 }}
             </Connect>
         )
