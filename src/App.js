5777a826075d89f13014607791b189dc54e5f993
yunganw <solariswu@gmail.com>
2019-12-05 11:02:22 +1100

    typo correction

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
                 <Switch>
                     {/* If the current URL is /about, this route is rendered
                         while the rest are ignored */}
-                    <Route path="synonymschoises/:session/:part"}>
+                    <Route path="synonymschoises/:session/:part">}>
                         <SynonymsChoises />
                     </Route>
 
