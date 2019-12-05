b6e1313af32df4297926e52299bb9ffce0d8944d
yunganw <solariswu@gmail.com>
2019-12-05 13:57:40 +1100

    test router on gh

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
         Amplify.Logger.LOG_LEVEL = 'VERBOSE';
 
         return (
-            <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <BrowserRouter>
                 <Switch>
                     <Route path="/synonymschoises/:session/:part">}>
                         <SynonymsChoises />
